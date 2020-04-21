21d39d3bef7faea19c00ad6bdb6f05e7b43aad20
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-21 17:51:31 -0300

    import gallery component

--- miguel-melo-design/src/views/Home.vue
+++ miguel-melo-design/src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="home">
-    <h1>Home view</h1>
+    <gallery/>
   </div>
 </template>
 
 <script>
 import setActive from '../components/mixins/ActiveNavItem';
+import gallery from '../components/Gallery.vue';
 
 export default {
   name: 'Home',
   mixins: [setActive],
+  components: {
+    gallery,
+  },
   data() {
     return {
       nomeRota: null,
