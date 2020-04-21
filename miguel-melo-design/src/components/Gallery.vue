d43a0901a07c651f3c9f5c50278e69e0a5dbe831
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-21 17:50:50 -0300

    add gallery structure

---
+++ miguel-melo-design/src/components/Gallery.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="row" id="gallery">
+    <div class="col-md-2 col-sm-12 no-spaces">
+      <card/>
+    </div>
+        <div class="col-md-2 col-sm-12 no-spaces">
+      <card/>
+    </div>
+        <div class="col-md-2 col-sm-12 no-spaces">
+      <card/>
+    </div>
+        <div class="col-md-2 col-sm-12 no-spaces">
+      <card/>
+    </div>
+        <div class="col-md-2 col-sm-12 no-spaces">
+      <card/>
+    </div>
+        <div class="col-md-2 col-sm-12 no-spaces">
+      <card/>
+    </div>
+        <div class="col-md-2 col-sm-12 no-spaces">
+      <card/>
+    </div>
+        <div class="col-md-2 col-sm-12 no-spaces">
+      <card/>
+    </div>
+        <div class="col-md-2 col-sm-12 no-spaces">
+      <card/>
+    </div>
+        <div class="col-md-2 col-sm-12 no-spaces">
+      <card/>
+    </div>
+        <div class="col-md-2 col-sm-12 no-spaces">
+      <card/>
+    </div>
+    <div class="col-md-2 col-sm-12 no-spaces">
+      <card/>
+    </div>
+  </section>
+</template>
+
+<script>
+import card from './page-components/ImageGallery.vue';
+
+export default {
+  name: 'gallery-home',
+  components: {
+    card,
+  },
+};
+</script>
