19325eb374f768674fe87da4bd237e223443a42c
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-20 23:37:04 -0300

    add routes

--- miguel-melo-design/src/router/index.js
+++ miguel-melo-design/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import About from '../views/Sobre.vue';
+import Contact from '../views/Contato.vue';
 
 Vue.use(VueRouter);
 
@@ -11,16 +13,27 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/sobre',
+    name: 'Sobre',
+    component: About,
   },
+  {
+    path: '/contato',
+    name: 'Contato',
+    component: Contact,
+  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
 });
 
