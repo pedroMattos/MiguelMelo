bb673e0a22335a89d194e4bdb43584e1e007fb86
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-20 23:36:57 -0300

    add active item script

---
+++ miguel-melo-design/src/components/mixins/ActiveNavItem.js
@@ -0,0 +1,23 @@
+export default {
+  mounted() {
+    const ctx = this;
+    this.$nextTick().then(() => {
+      this.nomeRota = this.$route.name;
+      function setActiveItem() {
+        const listItem = document.getElementsByClassName('list-item');
+        const routeN = ctx.nomeRota;
+        for (let i = 0; i < listItem.length; i++) {
+          const name = listItem[i].textContent;
+          if (listItem[i].textContent == routeN) {
+            listItem[i].classList.add('menu-item-active');
+          } else {
+            if (listItem[i].classList.contains('menu-item-active')) {
+              listItem[i].classList.remove('menu-item-active');
+            }
+          }
+        }
+      }
+      setActiveItem();
+    })
+  },
+};
