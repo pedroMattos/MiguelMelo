0b64821c076b86fb0fb2b2f699e482b69342002a
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-21 23:01:31 -0300

    add contact form structure

---
+++ miguel-melo-design/src/components/ContactForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <form id="contact-form" action="">
+    <div class="input-field">
+      <label class="label" for="name-client">Nome</label>
+      <input class="input" type="text" name="nameC" id="name-client">
+    </div>
+    <div class="input-field">
+      <label class="label" for="email-client">Email</label>
+      <input class="input" type="email" name="emailC" id="email-client">
+    </div>
+    <div class="input-field">
+      <label class="label" for="tel-client">Telefone</label>
+      <input class="input" type="tel" name="telC" id="tel-client">
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'contact-form',
+};
+</script>
+
+<style lang="scss">
+@import '../assets/scss/variables.scss';
+  #contact-form {
+    display: inline-flex;
+    .input-field {
+      position: relative;
+      label {
+        position: absolute;
+        font-family: 'Poppins', Arial, Helvetica, sans-serif;
+        color: $grey-form;
+        top: 0;
+        transition-duration: .5s;
+      }
+      input:-internal-autofill-selected {
+        background-color: transparent !important;
+        color: $grey-text !important;
+        -webkit-appearance: none !important;
+        background: none;
+      }
+      .input {
+        background-color: transparent;
+        border: none;
+        border-bottom: 1px solid $grey-form;
+        margin-right: 10px;
+        transition-duration: .5s;
+        width: 250px;
+        color: $grey-form;
+        height: 30px;
+        &:focus {
+          outline: none;
+          border-bottom: 1px solid rgba(255, 255, 255, 0.349);
+          transition-duration: .5s;
+        }
+      }
+    }
+  }
+</style>
