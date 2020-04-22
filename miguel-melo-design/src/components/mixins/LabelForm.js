d82b799456b35f0fe2a69fa70fa5c12f478f5a6a
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-21 23:01:40 -0300

    add label move function

---
+++ miguel-melo-design/src/components/mixins/LabelForm.js
@@ -0,0 +1,29 @@
+export default {
+  mounted() {
+    function labelMove() {
+      const label = document.getElementsByClassName('label');
+      const input = document.getElementsByClassName('input');
+      for(let i = 0; i < label.length; i++) {
+        label[i].addEventListener('click', (el) => {
+          el.target.classList.add('clicked');
+        })
+        input[i].addEventListener('click', (el) => {
+          label[i].classList.add('clicked');
+        })
+        input[i].addEventListener('blur', (el) => {
+          if(el.target.value.length <= 0) {
+            label[i].classList.remove('clicked');
+          } else {
+            label[i].classList.add('clicked');
+          }
+        })
+      }
+      // for(let i = 0; i < input.length; i++) {
+      //   input[i].addEventListener('keydown', (el) => {
+      //     label[i].classList.add('clicked')
+      //   })
+      // }
+    }
+    labelMove();
+  },
+};
