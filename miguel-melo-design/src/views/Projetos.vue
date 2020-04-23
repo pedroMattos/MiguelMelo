dcbeacedb194bb4760c7bd2a08e24903b445187c
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-04-23 10:34:30 -0300

    add projetos view

---
+++ miguel-melo-design/src/views/Projetos.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="" id="main-projeto">
+    <gallery/>
+  </section>
+</template>
+
+<script>
+import gallery from '../components/Gallery.vue';
+export default {
+  name: 'projeto',
+  components: {
+    gallery,
+  },
+};
+</script>
