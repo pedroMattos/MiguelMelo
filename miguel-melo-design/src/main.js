5e2311dcff12720ff040904ea18c206fde40477f
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-05-03 18:33:46 -0300

    import firebase

--- miguel-melo-design/src/main.js
+++ miguel-melo-design/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import firebase from './firebase/index';
 import './assets/scss/main.scss';
 
+Vue.use(firebase);
 Vue.config.productionTip = false;
 
 new Vue({
