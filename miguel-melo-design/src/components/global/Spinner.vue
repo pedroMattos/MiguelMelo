cde1912ae92b88f4bed6799cd3fc868015c929b4
Pedro Mattos <laranjeiraspedro@gmail.com>
2020-05-22 15:00:10 -0300

    add spinner component

---
+++ miguel-melo-design/src/components/global/Spinner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="spinner" v-if="visibility"></div>
+</template>
+
+<script>
+export default {
+  name: 'loader-spinner',
+  data() {
+    return {
+      visibility: true,
+    };
+  },
+  created() {
+    this.$root.$on('Spinner::show', () => {
+      this.visibility = true;
+    })
+    this.$root.$on('Spinner::hide', () => {
+      this.visibility = false;
+    })
+  },
+}
+</script>
